66bb59f3e0347b1bd4f379a26d30f835bffa3673
Kirill_fizmatovich <139997335+Kraillll@users.noreply.github.com>
2025-06-02 12:13:58 +0300

    Update Trigonometric_calculator.py

--- Trigonometric_calculator.py
+++ Trigonometric_calculator.py
@@ -1,46 +1,50 @@
 import math
 
 def func():
-    print("This is trigonometric calculator")
-    print("Aviable functions: cos, sin, tan, and also arcsin, arccos, arctan.")
+    try:
+        print("This is trigonometric calculator")
+        print("Aviable functions: cos, sin, tan, ctg, and also arcsin, arccos, arctan, arcctg.")
 
-    print(f"Enter type of function:", end="")
-    x = str(input())
-    print(f"Enter the measure of the angle:", end="")
-    angle = float(input())
-    if x in ["sin", "cos", "tan"]:
-        angle = math.radians(angle)
-        if (x == "sin"):
-            res = math.sin(angle)
+        print(f"Enter type of function:", end="")
+        x = str(input())
+        print(f"Enter the measure of the angle:", end="")
+        angle = float(input())
+        if x in ["sin", "cos", "tan", "ctg"]:
+            angle = math.radians(angle)
+            if (x == "sin"):
+                res = math.sin(angle)
+                print(res)
+
+            elif (x == "cos"):
+                res = math.cos(angle)
+                print(res)
+
+            elif (x == "tan"):
+                res = math.tan(angle)
+                print(res)
+
+            elif (x == "ctg"):
+                res = 1 / math.tan(angle)
+
+        elif (x == "arcsin"):
+            res = math.asin(angle)
             print(res)
+            if (angle < -1, angle > 1):
+                print("Math ERROR")
 
-        elif (x == "cos"):
-            res = math.cos(angle)
+        elif (x == "arccos"):
+            res = math.acos(angle)
             print(res)
+            if (angle < -1, angle > 1):
+                print("Math ERROR")
 
-        elif (x == "tan"):
-            res = math.tan(angle)
-            print(res)
+        elif (x == "arctan"):
+            res = math.atan(angle)
 
-        else:
-            print("Syntax ERROR")
+        elif (x == "arcctg"):
+            res = 1 / math.atan(angle)
 
-    elif (x == "arcsin"):
-        res = math.asin(angle)
-        print(res)
-        if (angle < -1, angle > 1):
-            print("Math ERROR")
-
-    elif (x == "arccos"):
-        res = math.acos(angle)
-        print(res)
-        if (angle < -1, angle > 1):
-            print("Math ERROR")
-
-    elif (x == "arctan"):
-        res = math.atan(angle)
-
-    else:
+    except:
         print("Syntax ERROR")
 
 func()
